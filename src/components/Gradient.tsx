033c18b1e1a71e8b3eb0b7a357b3ce36830dd917
Arthur Gamblin <arthur.gamblin@epitech.eu>
2023-11-08 18:57:21 +0100

    refactor(gradient): added easing

--- src/components/Gradient.tsx
+++ src/components/Gradient.tsx
@@ -10,7 +10,16 @@ function Gradient() {
     <div
       className="pointer-events-none fixed inset-0 z-30 lg:absolute"
       style={{
-        background: `radial-gradient(600px at ${x}px ${y}px, var(--gradient-color), transparent 80%)`,
+        background: `radial-gradient(600px at ${x}px ${y}px,
+             rgba(29, 78, 216, 0.15),
+             rgba(31, 75, 205, 0.15),
+             rgba(36, 69, 178, 0.15),
+             rgba(39, 61, 143, 0.15),
+             rgba(39, 52, 109, 0.15),
+             rgba(37, 43, 81, 0.15),
+             rgba(33, 37, 62, 0.15),
+             transparent 
+             )`,
       }}
     ></div>
   );
