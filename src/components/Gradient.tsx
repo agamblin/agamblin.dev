7ced7368dfaef2c92874a923154340b54b72e257
Arthur Gamblin <arthur.gamblin@epitech.eu>
2023-11-08 21:29:32 +0100

    fix(gradient): fixed moving gradient on coarse pointers

--- src/components/Gradient.tsx
+++ src/components/Gradient.tsx
@@ -8,20 +8,14 @@ function Gradient() {
 
   return (
     <div
-      className="pointer-events-none fixed inset-0 z-30 lg:absolute"
-      style={{
-        background: `radial-gradient(600px at ${x}px ${y}px,
-             rgba(29, 78, 216, 0.15),
-             rgba(31, 75, 205, 0.15),
-             rgba(36, 69, 178, 0.15),
-             rgba(39, 61, 143, 0.15),
-             rgba(39, 52, 109, 0.15),
-             rgba(37, 43, 81, 0.15),
-             rgba(33, 37, 62, 0.15),
-             transparent 
-             )`,
-      }}
-    ></div>
+      className="bg-spotlight pointer-events-none fixed inset-0 z-30 lg:absolute"
+      style={
+        {
+          '--dynamic-x': `${x}px`,
+          '--dynamic-y': `${y}px`,
+        } as React.CSSProperties
+      }
+    />
   );
 }
 
