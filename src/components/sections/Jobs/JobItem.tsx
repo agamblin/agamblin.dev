60f5800e9c851ce533819f5b3c747f07decc4f81
Arthur Gamblin <arthur.gamblin@epitech.eu>
2024-03-17 16:35:35 +0100

    fix(items): fixed wrong html semantic for  tags

--- src/components/sections/Jobs/JobItem.tsx
+++ src/components/sections/Jobs/JobItem.tsx
@@ -29,7 +29,6 @@ function formatDate(dateString: string) {
 function JobItem({
   title,
   company,
-  location,
   startDate,
   endDate,
   content,
@@ -37,34 +36,33 @@ function JobItem({
   companyUri,
 }: JobItemProps) {
   return (
-    <li>
+    <li className="group/item transition-[background-color, opacity]  relative grid items-baseline gap-2 rounded-md duration-500 hover:duration-[250ms] sm:grid-cols-10 sm:gap-8 md:gap-4 lg:p-4 lg:hover:bg-primary-900/40 lg:hover:!opacity-100 lg:hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:hover:drop-shadow-lg lg:group-hover/list:opacity-50">
       {/* Removed -m-4 from the className below for consistency with the other sections, check if fix is available to increase spacing*/}
       <a
-        className="group/item transition-[background-color, opacity]  grid items-baseline gap-2 rounded-md duration-500 hover:duration-[250ms] sm:grid-cols-10 sm:gap-8 md:gap-4 lg:p-4 lg:hover:bg-primary-900/40 lg:hover:!opacity-100 lg:hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:hover:drop-shadow-lg lg:group-hover/list:opacity-50"
         href={companyUri}
         aria-label={`Link to ${company} website`}
         target="_blank"
         rel="noreferrer"
-      >
-        <header className="text-xs font-semibold uppercase tracking-wide text-primary-400 sm:col-span-3">
-          <p>
-            {formatDate(startDate)} &#8212; {formatDate(endDate)}
-          </p>
-        </header>
-        <div className="flex flex-col gap-2 sm:col-span-7">
-          <h3 className="font-medium leading-none text-primary-100">
-            {title} • {company}
-          </h3>
-          <div className="hyphens-auto text-sm leading-normal">
-            <MDXRemote source={content} />
-          </div>
-          <ul className="mt-2 flex flex-wrap items-start gap-2">
-            {tags.map(tag => (
-              <Tag key={tag}>{tag}</Tag>
-            ))}
-          </ul>
+        className="absolute inset-0"
+      />
+      <header className="text-xs font-semibold uppercase tracking-wide text-primary-400 sm:col-span-3">
+        <p>
+          {formatDate(startDate)} &#8212; {formatDate(endDate)}
+        </p>
+      </header>
+      <div className="flex flex-col gap-2 sm:col-span-7">
+        <h3 className="font-medium leading-none text-primary-100">
+          {title} • {company}
+        </h3>
+        <div className="hyphens-auto text-sm leading-normal">
+          <MDXRemote source={content} />
         </div>
-      </a>
+        <ul className="mt-2 flex flex-wrap items-start gap-2">
+          {tags.map(tag => (
+            <Tag key={tag}>{tag}</Tag>
+          ))}
+        </ul>
+      </div>
     </li>
   );
 }
--- src/components/sections/Projects/ProjectItem.tsx
+++ src/components/sections/Projects/ProjectItem.tsx
@@ -34,41 +34,40 @@ function ProjectItem({
   content,
 }: ProjectItemProps) {
   return (
-    <li>
+    <li className="group/item transition-[background-color, opacity] relative grid grid-rows-[min-content_1fr_min-content] items-baseline gap-4 rounded-md  duration-500 hover:duration-[250ms] sm:grid-cols-10 sm:gap-x-8 sm:gap-y-2 md:gap-x-4  lg:p-4 lg:hover:bg-primary-900/40 lg:hover:!opacity-100 lg:hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:hover:drop-shadow-lg lg:group-hover/list:opacity-50">
       {/* Removed -m-4 from the className below for consistency with the other sections, check if fix is available to increase spacing*/}
       <a
-        className="group/item transition-[background-color, opacity] grid grid-rows-[min-content_1fr_min-content] items-baseline gap-4 rounded-md  duration-500 hover:duration-[250ms] sm:grid-cols-10 sm:gap-x-8 sm:gap-y-2 md:gap-x-4  lg:p-4 lg:hover:bg-primary-900/40 lg:hover:!opacity-100 lg:hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:hover:drop-shadow-lg lg:group-hover/list:opacity-50"
         href={link}
         aria-label={`Link to ${name} project`}
         target="_blank"
         rel="noreferrer"
-      >
-        <header className="-mb-2 flex h-full flex-col text-xs font-semibold uppercase tracking-wide text-primary-400 sm:col-span-3 sm:-mb-0">
-          <p>{formatDate(date)}</p>
-        </header>
-        <div className="sm:col-span-7">
-          <h3 className="font-medium leading-none text-primary-100">{name}</h3>
-        </div>
-        <div className="relative self-center sm:col-span-3 sm:max-w-[10rem]">
-          <Image
-            src={thumbnail}
-            alt={name}
-            width={16}
-            height={9}
-            loading="lazy"
-            layout="responsive"
-            className="rounded-sm object-cover"
-          />
-        </div>
-        <div className="hyphens-auto text-sm leading-normal sm:col-span-7 sm:col-start-4">
-          <MDXRemote source={content} />
-        </div>
-        <ul className="flex flex-wrap items-start gap-2 sm:col-span-7 sm:col-start-4 sm:mt-2">
-          {tags.map(tag => (
-            <Tag key={tag}>{tag}</Tag>
-          ))}
-        </ul>
-      </a>
+        className="absolute inset-0"
+      />
+      <header className="-mb-2 flex h-full flex-col text-xs font-semibold uppercase tracking-wide text-primary-400 sm:col-span-3 sm:-mb-0">
+        <p>{formatDate(date)}</p>
+      </header>
+      <div className="sm:col-span-7">
+        <h3 className="font-medium leading-none text-primary-100">{name}</h3>
+      </div>
+      <div className="relative self-center sm:col-span-3 sm:max-w-[10rem]">
+        <Image
+          src={thumbnail}
+          alt={name}
+          width={16}
+          height={9}
+          loading="lazy"
+          layout="responsive"
+          className="rounded-sm object-cover"
+        />
+      </div>
+      <div className="hyphens-auto text-sm leading-normal sm:col-span-7 sm:col-start-4">
+        <MDXRemote source={content} />
+      </div>
+      <ul className="flex flex-wrap items-start gap-2 sm:col-span-7 sm:col-start-4 sm:mt-2">
+        {tags.map(tag => (
+          <Tag key={tag}>{tag}</Tag>
+        ))}
+      </ul>
     </li>
   );
 }
