6886307e15bc0ca0300e2b33eb76482b4bdb9f40
Arthur Gamblin <arthur.gamblin@epitech.eu>
2024-03-28 12:57:12 +0100

    refactor(spotlight): extracted motion logic inside custom hook

--- src/components/Spotlight.tsx
+++ src/components/Spotlight.tsx
@@ -7,6 +7,7 @@ import {
   useMotionTemplate,
   useMotionValue,
   useSpring,
+  MotionValue,
 } from 'framer-motion';
 
 const SPRING_CONFIG = {
@@ -14,9 +15,29 @@ const SPRING_CONFIG = {
   stiffness: 100,
 } satisfies SpringOptions;
 
+function useSpotlightBackground(
+  mouseX: MotionValue<number>,
+  mouseY: MotionValue<number>,
+) {
+  const xSpring = useSpring(mouseX, SPRING_CONFIG);
+  const ySpring = useSpring(mouseY, SPRING_CONFIG);
+
+  const background = useMotionTemplate`
+    radial-gradient(
+      650px at ${xSpring}px ${ySpring}px,
+      rgba(29, 78, 216, 0.15), rgba(31, 75, 205, 0.15), rgba(36, 69, 178, 0.15),
+      rgba(39, 61, 143, 0.15), rgba(39, 52, 109, 0.15), rgba(37, 43, 81, 0.15),
+      rgba(33, 37, 62, 0.15), transparent
+    )
+  `;
+
+  return background;
+}
+
 function SpotlightOverlay({ children }: React.PropsWithChildren) {
-  let mouseX = useMotionValue(0);
-  let mouseY = useMotionValue(0);
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
+  const spotlightBackground = useSpotlightBackground(mouseX, mouseY);
 
   function handleMouseMove({ currentTarget, clientX, clientY }: MouseEvent) {
     let { left, top } = currentTarget.getBoundingClientRect();
@@ -33,22 +54,7 @@ function SpotlightOverlay({ children }: React.PropsWithChildren) {
       <motion.div
         className="pointer-events-none fixed inset-0 lg:absolute"
         style={{
-          background: useMotionTemplate`
-            radial-gradient(
-              650px at ${useSpring(mouseX, SPRING_CONFIG)}px ${useSpring(
-                mouseY,
-                SPRING_CONFIG,
-              )}px,
-              rgba(29, 78, 216, 0.15),
-              rgba(31, 75, 205, 0.15),
-              rgba(36, 69, 178, 0.15),
-              rgba(39, 61, 143, 0.15),
-              rgba(39, 52, 109, 0.15),
-              rgba(37, 43, 81, 0.15),
-              rgba(33, 37, 62, 0.15),
-              transparent
-            )
-        `,
+          background: spotlightBackground,
         }}
       />
       {children}
