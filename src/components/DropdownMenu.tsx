6f53621619e919a7c87bda3c15f936ff09c68118
Arthur Gamblin <arthur.gamblin@epitech.eu>
2025-03-14 15:28:08 +0100

    feat(dropdown): added basic menu animation

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -9,9 +9,16 @@ import {
 } from '@radix-ui/react-dropdown-menu';
 import { FileText } from 'react-feather';
 
+import { AnimatePresence, motion } from 'framer-motion';
+import { useState } from 'react';
+
+const AnimatedContent = motion(Content);
+
 function DropdownMenu() {
+  const [isOpen, setOpen] = useState(false);
+
   return (
-    <Root>
+    <Root open={isOpen} onOpenChange={setOpen}>
       <Trigger
         className={`
           after:content-['
@@ -27,28 +34,39 @@ function DropdownMenu() {
       >
         <FileText size={20} />
       </Trigger>
-      <Portal>
-        <Content className="rounded-md bg-primary-900 shadow-xl">
-          <Item
-            className={`
-              relative flex items-center px-2 py-1 font-semibold text-foreground-darker
-              before:mr-2 before:content-['🇫🇷']
-              hover:text-foreground-lighter
-            `}
-          >
-            FR
-          </Item>
-          <Item
-            className={`
-              relative flex items-center px-2 py-1 font-semibold text-foreground-darker
-              before:mr-2 before:content-['🇺🇸']
-              hover:text-foreground-lighter
-            `}
-          >
-            EN
-          </Item>
-        </Content>
-      </Portal>
+      <AnimatePresence>
+        {isOpen && (
+          <Portal forceMount>
+            <Content className="rounded-md bg-primary-900 shadow-md" asChild>
+              <motion.div
+                transition={{ type: 'spring', duration: 0.3, bounce: 0.3 }}
+                initial={{ opacity: 0, scale: 0.85 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0.85 }}
+              >
+                <Item
+                  className={`
+                    relative flex items-center px-2 py-1 font-semibold text-foreground-darker
+                    before:mr-2 before:content-['🇫🇷']
+                    hover:text-foreground-lighter
+                  `}
+                >
+                  FR
+                </Item>
+                <Item
+                  className={`
+                    relative flex items-center px-2 py-1 font-semibold text-foreground-darker
+                    before:mr-2 before:content-['🇺🇸']
+                    hover:text-foreground-lighter
+                  `}
+                >
+                  EN
+                </Item>
+              </motion.div>
+            </Content>
+          </Portal>
+        )}
+      </AnimatePresence>
     </Root>
   );
 }
